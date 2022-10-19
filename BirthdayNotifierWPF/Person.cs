a8a971f72d1dcd639da13a7fcb875a09fe2fb449
Pepa <josef.panek@gmail.com>
2022-10-20 00:32:50 +0200

    development

---
+++ BirthdayNotifierWPF/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BirthdayNotifierWPF
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Age
+        {
+            get
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - Birthday.Year;
+
+                if (today < Birthday.AddYears(age))
+                    age--;
+                return age;
+            }
+        }
+        public int DaysRemaining
+        {
+            get
+            {
+                DateTime nextBirthday = Birthday.AddYears(Age + 1);
+
+                TimeSpan timeSpan = nextBirthday - DateTime.Today;
+
+                return Convert.ToInt32(timeSpan.TotalDays);
+            }
+        }
+        public Person(string name, DateTime birthday)
+        {
+            Name = name;
+            Birthday = birthday;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ BirthdayNotifierWPF/PersonsAdministrator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirthdayNotifierWPF
+{
+    public class PersonsAdministrator
+    {
+        public ObservableCollection<Person> Persons { get; set; }
+        public Person NextPerson { get; set; }
+        public DateTime Today
+        {
+            get
+            {
+                return DateTime.Now;
+            }
+        }
+        public PersonsAdministrator()
+        {
+            Persons = new ObservableCollection<Person>();
+        }
+        private void FindNext()
+        {
+            var sortedPersons = Persons.OrderBy(o => o.Birthday);
+            if (sortedPersons.Any())
+                NextPerson = sortedPersons.First();
+            else
+                NextPerson = null;
+        }
+    }
+}
